ce3749c70029c96fb25f3d9640cf20c7598bed68
Emmanuel Deschamps <ezka@desformes.net>
2013-10-03 10:53:52 +0200

    all stuff

---
+++ converted-html/assets/scripts/app.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	/*
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_minimal',
+    radioClass: 'iradio_minimal',
+    increaseArea: '20%' // optional
+  });
+*/
+});
